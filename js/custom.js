6a0c990fd7a9ab8b803e5be3a9750028ec80836d
Maxwell kemboi <43989639+maxisz@users.noreply.github.com>
2021-12-06 17:17:31 +0300

    re-upload

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,9 @@
-
-
 const name = window.prompt('hey whats your name?');
 document.getElementById('header').innerHTML = `Welcome ${name}`;
+
+
+
+
+var today = new Date();
+var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+document.getElementById('date').innerHTML = "today " + date;
